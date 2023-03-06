43ff33a2f5de701553092a17f30ca6e8b20713b5
mayur <mayurpimpude@gmail.com>
2023-03-06 15:23:07 +0530

    intial

--- app.py
+++ app.py
@@ -1,3 +1,38 @@
-import streamlit as st
-from flask import Flask, render_template,request,redirect
+from fastapi import FastAPI,Request,Form
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse('index.html',{"request": request})
+
+@app.get("/input", response_class=HTMLResponse)
+async def input(request: Request):
+    return templates.TemplateResponse('input.html',{"request": request})
+
+@app.post("/input")
+async def input1(tex: str = Form(...)):
+    print(tex)
+    #return templates.TemplateResponse('input.html',{"request": Request})
+    return tex
+
+# from fastapi import FastAPI, Form, Request
+# from fastapi.responses import HTMLResponse
+# from fastapi.templating import Jinja2Templates
+
+# app = FastAPI()
+# templates = Jinja2Templates(directory='templates')
+
+# @app.post('/disable')
+# def disable_cat(cat_name: str = Form(...)):
+#     return f'{cat_name}'
+
+# @app.get('/', response_class=HTMLResponse)
+# def main(request: Request):
+#     return templates.TemplateResponse('index2.html', {'request': request})
